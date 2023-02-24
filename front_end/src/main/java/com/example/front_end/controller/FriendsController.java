409b3759ddcb92b17de0097e80f48e4e8408c363
loc <nguyengialoc7@gmail.com>
2023-02-24 18:16:55 +0700

    Friends Controller

--- front_end/src/main/java/com/example/front_end/controller/FriendsController.java
+++ front_end/src/main/java/com/example/front_end/controller/FriendsController.java
@@ -45,6 +45,7 @@ public class FriendsController implements Initializable{
             user.setName((String) rspItem.getData().get(1));
             user.setAvatar((String) rspItem.getData().get(2));
             userList.add(user);
+            System.out.println(user);
         }
         return userList;
     }
@@ -104,7 +105,7 @@ public class FriendsController implements Initializable{
         friendListView.setItems(userList);
 
     }
-    public void followingOnClick(MouseEvent e){
+    public void followingOnClick(MouseEvent e) throws IOException {
         followingBtn.setStyle("-fx-font-weight: bold;");
         friendsBtn.setStyle("-fx-font-weight: normal;");
         followerBtn.setStyle("-fx-font-weight: normal;");
@@ -113,41 +114,23 @@ public class FriendsController implements Initializable{
         followerBtn.setTextFill(Paint.valueOf("#000"));
         friendListView.setCellFactory(param -> new UserCell(2));
         ObservableList<User> userList = FXCollections.observableArrayList();
-        userList.add(new User(1,
-                "Ta Quang Linh",
-                "https://toigingiuvedep.vn/wp-content/uploads/2022/01/anh-meo-cute.jpg",
-                "Toi la Ling",
-                "Nam",
-                null,
-                null,
-                "Bong da"));
-        userList.add(new User(2,
-                "hoang Anh Tuan",
-                "https://demoda.vn/wp-content/uploads/2022/02/anh-meo-cute-dang-yeu.jpeg",
-                "Toi la Ling",
-                "Nam",
-                null,
-                null,
-                "Bong da"));
-        userList.add(new User(3,
-                "Le Viet Hung",
-                "https://static.wixstatic.com/media/9d8ed5_b8fcc13f08fc4374bb1f979e032b0eb7~mv2.jpg/v1/fit/w_600,h_567,al_c,q_20,enc_auto/file.jpg",
-                "Toi la Ling",
-                "Nam",
-                null,
-                null,
-                "Bong da"));
-        userList.add(new User(1,
-                "Mai Dao Tuan Thanh",
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShFksgubjvBsya7NsCbgby4hskXh3UBIr97uWRIoM&s",
-                "Toi la Ling",
-                "Nam",
-                null,
-                null,
-                "Bong da"));
+
+        Data data = new Data(AppUtils.MessageCode.FOLLOWING);
+        data.getData().add(1);
+        AppUtils.sendData(clientSock, data);
+        Data response = AppUtils.recvData(clientSock); // receive count
+        int count = Integer.valueOf((String) response.getData().get(0));
+        System.out.println(response);
+        if(response.getMessageCode() == OK ) {
+            userList = getUserList(count);
+        }
+        else if(response.getMessageCode() == ERROR) {
+            exit(-1);
+        }
+
         friendListView.setItems(userList);
     }
-    public void followerOnClick(MouseEvent e){
+    public void followerOnClick(MouseEvent e) throws IOException {
         followerBtn.setStyle("-fx-font-weight: bold;");
         friendsBtn.setStyle("-fx-font-weight: normal;");
         followingBtn.setStyle("-fx-font-weight: normal;");
@@ -156,38 +139,19 @@ public class FriendsController implements Initializable{
         followerBtn.setTextFill(Paint.valueOf("#3465A4"));
         friendListView.setCellFactory(param -> new UserCell(3));
         ObservableList<User> userList = FXCollections.observableArrayList();
-        userList.add(new User(1,
-                "Ta Quang Linh",
-                "https://toigingiuvedep.vn/wp-content/uploads/2022/01/anh-meo-cute.jpg",
-                "Toi la Ling",
-                "Nam",
-                null,
-                null,
-                "Bong da"));
-        userList.add(new User(2,
-                "hoang Anh Tuan",
-                "https://demoda.vn/wp-content/uploads/2022/02/anh-meo-cute-dang-yeu.jpeg",
-                "Toi la Ling",
-                "Nam",
-                null,
-                null,
-                "Bong da"));
-        userList.add(new User(3,
-                "Le Viet Hung",
-                "https://static.wixstatic.com/media/9d8ed5_b8fcc13f08fc4374bb1f979e032b0eb7~mv2.jpg/v1/fit/w_600,h_567,al_c,q_20,enc_auto/file.jpg",
-                "Toi la Ling",
-                "Nam",
-                null,
-                null,
-                "Bong da"));
-        userList.add(new User(1,
-                "Mai Dao Tuan Thanh",
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShFksgubjvBsya7NsCbgby4hskXh3UBIr97uWRIoM&s",
-                "Toi la Ling",
-                "Nam",
-                null,
-                null,
-                "Bong da"));
+
+        Data data = new Data(AppUtils.MessageCode.FOLLOWER);
+        data.getData().add(1);
+        AppUtils.sendData(clientSock, data); // send data
+        Data response = AppUtils.recvData(clientSock); // receive count
+        int count = Integer.valueOf((String) response.getData().get(0));
+        System.out.println(response);
+        if (response.getMessageCode() == OK){
+            userList = getUserList(count);
+        }else if (response.getMessageCode() == ERROR){
+            exit(-1);
+        }
+
         friendListView.setItems(userList);
     }
     @SneakyThrows
@@ -198,9 +162,9 @@ public class FriendsController implements Initializable{
         friendListView.setCellFactory(param -> new UserCell(1));
         ObservableList<User> userList = FXCollections.observableArrayList();
 
-        Data data = new Data(AppUtils.MessageCode.FRIENDS);
-        data.getData().add(1);
-        AppUtils.sendData(clientSock, data); // send data
+        Data request = new Data(AppUtils.MessageCode.FRIENDS);
+        request.getData().add(1);
+        AppUtils.sendData(clientSock, request); // send data
         Data response = AppUtils.recvData(clientSock); // recv count
         int count = Integer.valueOf((String) response.getData().get(0));
         System.out.println(response);
--- front_end/src/main/java/com/example/front_end/controller/NotificationController.java
+++ front_end/src/main/java/com/example/front_end/controller/NotificationController.java
@@ -1,4 +1,5 @@
 package com.example.front_end.controller;
 
 public class NotificationController {
+
 }
