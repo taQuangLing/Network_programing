c4246ec8f39cb62752d7f36237a42565d1765e05
taQuangLing <linh.tq194605@gmail.com>
2023-02-24 15:24:45 +0700

    UPDATE add id, token

--- front_end/src/main/java/com/example/front_end/appUtils/GlobalVariable.java
+++ front_end/src/main/java/com/example/front_end/appUtils/GlobalVariable.java
@@ -6,10 +6,30 @@ import javafx.scene.Scene;
 public class GlobalVariable {
     private static GlobalVariable instance = new GlobalVariable();
     private ScreenController screenController;
+    private Integer id;
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    private String token;
 
     public GlobalVariable() {
         screenController = new ScreenController();
     }
+
 
     public static GlobalVariable getInstance() {
         return instance;
