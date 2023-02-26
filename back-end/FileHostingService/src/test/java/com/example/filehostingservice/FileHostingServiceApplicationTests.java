eb4be537bb5fa1e8b9af89d360ed5006bad67b85
taQuangLing <linh.tq194605@gmail.com>
2023-02-26 21:39:00 +0700

    create File Hosting service

---
+++ back-end/FileHostingService/src/test/java/com/example/filehostingservice/FileHostingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filehostingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileHostingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
